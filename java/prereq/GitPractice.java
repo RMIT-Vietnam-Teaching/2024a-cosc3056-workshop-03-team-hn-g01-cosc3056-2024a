2e2cf09c6f0c69677dcb241e48e8c4b5f5853276
sebastianz3 <hieuovw@gmail.com>
2024-05-10 08:53:49 +0700

    Add member name

--- java/prereq/GitPractice.java
+++ java/prereq/GitPractice.java
@@ -21,7 +21,7 @@ public class GitPractice {
    }
 
    public static void groupMember2() {
-      // TODO: Group Member 2, place your code here
+      System.out.println("Hieu");
    }
 
    public static void groupMember3() {
